ae106d527928548de862c30dc833d346bfe34a99
Manuel Outeiriño <manoutbar@alum.us.es>
2020-11-18 19:30:49 +0100

    #34 Exercise entitiy
    Exercise entity mapping

---
+++ src/main/java/org/springframework/samples/petclinic/model/Exercise.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.model;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "exercise")
+public class Exercise extends BaseEntity {
+	
+	String name;
+	
+	String description;
+	
+	String image;
+	
+	Integer numReps;
+	
+	Integer time;
+	
+	@ManyToOne
+	@JoinColumn(name = "type")
+	private ExerciseType type;
+	
+}
