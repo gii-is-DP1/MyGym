efb506dd822f57b929a986d5768d70fd7c876f23
Manuel Outeiriño <manoutbar@alum.us.es>
2020-11-29 22:50:45 +0100

    #34 Camino de entidad entrenamiento
    Inclusión de los tests para las historias de usuario HU1 y HU2

--- src/test/java/org/springframework/samples/petclinic/service/WorkoutServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/WorkoutServiceTests.java
@@ -17,24 +17,17 @@ package org.springframework.samples.petclinic.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
 import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.dao.DataAccessException;
-import org.springframework.samples.petclinic.model.Owner;
-import org.springframework.samples.petclinic.model.Pet;
-import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.model.Vet;
-import org.springframework.samples.petclinic.model.Visit;
-import org.springframework.samples.petclinic.model.User;
-import org.springframework.samples.petclinic.service.exceptions.DuplicatedPetNameException;
+import org.springframework.samples.petclinic.model.Exercise;
+import org.springframework.samples.petclinic.model.ExerciseType;
+import org.springframework.samples.petclinic.model.Training;
 import org.springframework.samples.petclinic.util.EntityUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -71,157 +64,98 @@ import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 class WorkoutServiceTests {        
-        @Autowired
+    
+	@Autowired
 	protected WorkoutService workoutService;
         
-        @Autowired
-	protected OwnerService ownerService;	
-
-	/*@Test
-	void shouldFindPetWithCorrectId() {
-		Pet pet7 = this.exerciseService.findPetById(7);
-		assertThat(pet7.getName()).startsWith("Samantha");
-		assertThat(pet7.getOwner().getFirstName()).isEqualTo("Jean");
-
+    @Autowired
+	protected OwnerService ownerService;
+    
+    @Test
+	void shouldFindExerciseWithCorrectId() {
+    	Exercise exercise = this.workoutService.findExerciseById(1);
+    	
+    	assertThat(exercise.getName()).startsWith("Abdominales");
+    	assertThat(exercise.getType().getName()).isEqualTo("repetitive");
 	}
-
-	@Test
-	void shouldFindAllPetTypes() {
-		Collection<PetType> petTypes = this.petService.findPetTypes();
-
-		PetType petType1 = EntityUtils.getById(petTypes, PetType.class, 1);
-		assertThat(petType1.getName()).isEqualTo("cat");
-		PetType petType4 = EntityUtils.getById(petTypes, PetType.class, 4);
-		assertThat(petType4.getName()).isEqualTo("snake");
+    
+    @Test
+	void shouldFindAllExerciseTypes() {
+    	Collection<ExerciseType> exerciseTypes = this.workoutService.findExerciseTypes();
+    	
+    	ExerciseType type1 = EntityUtils.getById(exerciseTypes, ExerciseType.class, 1);
+    	ExerciseType type2 = EntityUtils.getById(exerciseTypes, ExerciseType.class, 2);
+    	
+		assertThat(type1.getName()).isEqualTo("temporary");
+		assertThat(type2.getName()).isEqualTo("repetitive");
 	}
-
-	@Test
+    
+    @Test
 	@Transactional
-	public void shouldInsertPetIntoDatabaseAndGenerateId() {
-		Owner owner6 = this.ownerService.findOwnerById(6);
-		int found = owner6.getPets().size();
+	public void shouldInsertExerciseIntoDatabaseAndGenerateId() {
+    	Collection<ExerciseType> exerciseTypes = this.workoutService.findExerciseTypes();
 
-		Pet pet = new Pet();
-		pet.setName("bowser");
-		Collection<PetType> types = this.petService.findPetTypes();
-		pet.setType(EntityUtils.getById(types, PetType.class, 2));
-		pet.setBirthDate(LocalDate.now());
-		owner6.addPet(pet);
-		assertThat(owner6.getPets().size()).isEqualTo(found + 1);
-
-            try {
-                this.petService.savePet(pet);
-            } catch (DuplicatedPetNameException ex) {
-                Logger.getLogger(WorkoutServiceTests.class.getName()).log(Level.SEVERE, null, ex);
-            }
-		this.ownerService.saveOwner(owner6);
-
-		owner6 = this.ownerService.findOwnerById(6);
-		assertThat(owner6.getPets().size()).isEqualTo(found + 1);
-		// checks that id has been generated
-		assertThat(pet.getId()).isNotNull();
-	}
-	
-	@Test
-	@Transactional
-	public void shouldThrowExceptionInsertingPetsWithTheSameName() {
-		Owner owner6 = this.ownerService.findOwnerById(6);
-		Pet pet = new Pet();
-		pet.setName("wario");
-		Collection<PetType> types = this.petService.findPetTypes();
-		pet.setType(EntityUtils.getById(types, PetType.class, 2));
-		pet.setBirthDate(LocalDate.now());
-		owner6.addPet(pet);
-		try {
-			petService.savePet(pet);		
-		} catch (DuplicatedPetNameException e) {
-			// The pet already exists!
-			e.printStackTrace();
-		}
+		Exercise exercise = new Exercise();
+		exercise.setName("Burpees round");
+		exercise.setDescription("Ronda completa de burpees sin descanso intermedio");
+		exercise.setType(EntityUtils.getById(exerciseTypes, ExerciseType.class, 2));
+		exercise.setNumReps(20);
 		
-		Pet anotherPetWithTheSameName = new Pet();		
-		anotherPetWithTheSameName.setName("wario");
-		anotherPetWithTheSameName.setType(EntityUtils.getById(types, PetType.class, 1));
-		anotherPetWithTheSameName.setBirthDate(LocalDate.now().minusWeeks(2));
-		Assertions.assertThrows(DuplicatedPetNameException.class, () ->{
-			owner6.addPet(anotherPetWithTheSameName);
-			petService.savePet(anotherPetWithTheSameName);
-		});		
+        try {
+            this.workoutService.saveExercise(exercise);
+        } catch (Exception ex) {
+            Logger.getLogger(WorkoutServiceTests.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+		Collection<Exercise> searchResultCollection = this.workoutService.findExercises("Burpees round");
+		assertThat(searchResultCollection.size()).isEqualTo(1);
+		assertThat(searchResultCollection.iterator().next().getId()).isNotNull();
 	}
-
-	@Test
-	@Transactional
-	public void shouldUpdatePetName() throws Exception {
-		Pet pet7 = this.petService.findPetById(7);
-		String oldName = pet7.getName();
-
-		String newName = oldName + "X";
-		pet7.setName(newName);
-		this.petService.savePet(pet7);
-
-		pet7 = this.petService.findPetById(7);
-		assertThat(pet7.getName()).isEqualTo(newName);
+    
+    @Test
+	void shouldFindTrainingWithCorrectId() {
+    	Training training = this.workoutService.findTrainingById(1);
+    	
+    	assertThat(training.getName()).startsWith("Circuito");
 	}
-	
-	@Test
+    
+    @Test
 	@Transactional
-	public void shouldThrowExceptionUpdatingPetsWithTheSameName() {
-		Owner owner6 = this.ownerService.findOwnerById(6);
-		Pet pet = new Pet();
-		pet.setName("wario");
-		Collection<PetType> types = this.petService.findPetTypes();
-		pet.setType(EntityUtils.getById(types, PetType.class, 2));
-		pet.setBirthDate(LocalDate.now());
-		owner6.addPet(pet);
+	public void shouldInsertTrainingIntoDatabaseAndGenerateId() {
+		Exercise exercise = this.workoutService.findExerciseById(1);
+
+		Training training = new Training();
+		training.setName("Rutina de iniciación");
+		training.setDescription("Circuito de iniciación para usuarios principiantes");
 		
-		Pet anotherPet = new Pet();		
-		anotherPet.setName("waluigi");
-		anotherPet.setType(EntityUtils.getById(types, PetType.class, 1));
-		anotherPet.setBirthDate(LocalDate.now().minusWeeks(2));
-		owner6.addPet(anotherPet);
+		training.addExercise(exercise);
 		
-		try {
-			petService.savePet(pet);
-			petService.savePet(anotherPet);
-		} catch (DuplicatedPetNameException e) {
-			// The pets already exists!
-			e.printStackTrace();
-		}				
-			
-		Assertions.assertThrows(DuplicatedPetNameException.class, () ->{
-			anotherPet.setName("wario");
-			petService.savePet(anotherPet);
-		});		
-	}
+		assertThat(training.getExercises().size()).isEqualTo(1);
+        this.workoutService.saveTraining(training);
 
-	@Test
+
+		training = this.workoutService.findTrainingById(training.getId());
+		assertThat(training.getExercises().size()).isEqualTo(1);
+		assertThat(training.getId()).isNotNull();
+	}
+    
+    @Test
 	@Transactional
-	public void shouldAddNewVisitForPet() {
-		Pet pet7 = this.petService.findPetById(7);
-		int found = pet7.getVisits().size();
-		Visit visit = new Visit();
-		pet7.addVisit(visit);
-		visit.setDescription("test");
-		this.petService.saveVisit(visit);
-            try {
-                this.petService.savePet(pet7);
-            } catch (DuplicatedPetNameException ex) {
-                Logger.getLogger(WorkoutServiceTests.class.getName()).log(Level.SEVERE, null, ex);
-            }
+	public void shouldFindTrainingsByName() {
+		Exercise exercise = this.workoutService.findExerciseById(1);
 
-		pet7 = this.petService.findPetById(7);
-		assertThat(pet7.getVisits().size()).isEqualTo(found + 1);
-		assertThat(visit.getId()).isNotNull();
+		Training training = new Training();
+		training.setName("Rutina de iniciación");
+		training.setDescription("Circuito de iniciación para usuarios principiantes");
+		training.addExercise(exercise);
+        this.workoutService.saveTraining(training);
+
+
+		Collection<Training> searchResultCollection = this.workoutService.findTrainingsByName("Rutina");
+		assertThat(searchResultCollection.size()).isGreaterThan(0);
+
+		searchResultCollection = this.workoutService.findTrainingsByName("anInventedWorkoutName");
+		assertThat(searchResultCollection.size()).isEqualTo(0);
 	}
-
-	@Test
-	void shouldFindVisitsByPetId() throws Exception {
-		Collection<Visit> visits = this.petService.findVisitsByPetId(7);
-		assertThat(visits.size()).isEqualTo(2);
-		Visit[] visitArr = visits.toArray(new Visit[visits.size()]);
-		assertThat(visitArr[0].getPet()).isNotNull();
-		assertThat(visitArr[0].getDate()).isNotNull();
-		assertThat(visitArr[0].getPet().getId()).isEqualTo(7);
-	}*/
 
 }
